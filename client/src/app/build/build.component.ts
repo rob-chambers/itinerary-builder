d1c9858ae29cd63ea07f33b0c3fdfb9d50476787
Robert Chambers <rechambers11@gmail.com>
2018-08-28 20:29:42 +0100

    Added Bootstrap, got basic calendar function working

--- client/src/app/build/build.component.ts
+++ client/src/app/build/build.component.ts
@@ -1,7 +1,55 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { CalendarEvent, CalendarEventTimesChangedEvent } from 'angular-calendar';
+import { CalendarEvent, CalendarMonthViewDay, CalendarEventTimesChangedEvent } from 'angular-calendar';
 import { Subject } from 'rxjs';
 //import { colors } from '../demo-utils/colors';
+import {
+  subMonths,
+  addMonths,
+  addDays,
+  addWeeks,
+  subDays,
+  subWeeks,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  startOfDay,
+  endOfDay
+} from 'date-fns';
+
+type CalendarPeriod = 'day' | 'week' | 'month';
+
+function addPeriod(period: CalendarPeriod, date: Date, amount: number): Date {
+  return {
+    day: addDays,
+    week: addWeeks,
+    month: addMonths
+  }[period](date, amount);
+}
+
+function subPeriod(period: CalendarPeriod, date: Date, amount: number): Date {
+  return {
+    day: subDays,
+    week: subWeeks,
+    month: subMonths
+  }[period](date, amount);
+}
+
+function startOfPeriod(period: CalendarPeriod, date: Date): Date {
+  return {
+    day: startOfDay,
+    week: startOfWeek,
+    month: startOfMonth
+  }[period](date);
+}
+
+function endOfPeriod(period: CalendarPeriod, date: Date): Date {
+  return {
+    day: endOfDay,
+    week: endOfWeek,
+    month: endOfMonth
+  }[period](date);
+}
 
 @Component({
   selector: 'app-build',
@@ -10,16 +58,6 @@ import { Subject } from 'rxjs';
   styleUrls: ['./build.component.css']
 })
 export class BuildComponent {
-
-  BuildComponent() {
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth();
-    var day = d.getDate();
-    var maxDate = new Date(year + 2, month, day);
-
-    this.maxDate = maxDate;
-  }
 
   colors: any = {
     red: {
@@ -35,6 +73,20 @@ export class BuildComponent {
       secondary: '#FDF1BA'
     }
   };
+
+  constructor() {
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth();
+    var day = d.getDate();
+    var maxDate = new Date(year + 2, month, day);
+
+    this.maxDate = maxDate;
+
+    this.dateOrViewChanged();
+  }
+
+
 
   //view: string = 'month';
 
@@ -53,7 +105,7 @@ export class BuildComponent {
   //  this.refresh.next();
   //}
 
-  view: string = 'month';
+  view: CalendarPeriod = 'month';
   viewDate: Date = new Date();
   minDate: Date = new Date(this.viewDate);
   maxDate: Date;
@@ -83,4 +135,57 @@ export class BuildComponent {
     event.end = newEnd;
     this.refresh.next();
   }
+
+  prevBtnDisabled: boolean = false;
+
+  nextBtnDisabled: boolean = false;
+
+  increment(): void {
+    this.changeDate(addPeriod(this.view, this.viewDate, 1));
+  }
+
+  decrement(): void {
+    this.changeDate(subPeriod(this.view, this.viewDate, 1));
+  }
+
+  today(): void {
+    this.changeDate(new Date());
+  }
+
+  dateIsValid(date: Date): boolean {
+    return date >= this.minDate && date <= this.maxDate;
+  }
+
+  changeDate(date: Date): void {
+    this.viewDate = date;
+    this.dateOrViewChanged();
+  }
+
+  changeView(view: CalendarPeriod): void {
+    this.view = view;
+    this.dateOrViewChanged();
+  }
+
+  dateOrViewChanged(): void {
+    this.prevBtnDisabled = !this.dateIsValid(
+      endOfPeriod(this.view, subPeriod(this.view, this.viewDate, 1))
+    );
+    this.nextBtnDisabled = !this.dateIsValid(
+      startOfPeriod(this.view, addPeriod(this.view, this.viewDate, 1))
+    );
+    if (this.viewDate < this.minDate) {
+      this.changeDate(this.minDate);
+    } else if (this.viewDate > this.maxDate) {
+      this.changeDate(this.maxDate);
+    }
+  }
+
+  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    body.forEach(day => {
+      if (!this.dateIsValid(day.date)) {
+        day.cssClass = 'cal-disabled';
+      }
+    });
+  }
+
 }
